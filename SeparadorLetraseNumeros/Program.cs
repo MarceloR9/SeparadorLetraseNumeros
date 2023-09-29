c0ef9bce081109f0968277d04090cd5338be9492
Marcelo Rodrigues <marcelo.mdsr09@gmail.com>
2023-09-29 00:16:01 -0300

    Adicionar arquivos de projeto.

---
+++ SeparadorLetraseNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma linha com letras e números:");
+        string entrada = Console.ReadLine();
+
+        SepararLetrasNumeros(entrada);
+    }
+
+    static void SepararLetrasNumeros(string texto)
+    {
+        string padraoLetras = "[A-Za-z]";
+        string padraoNumeros = "[0-9]";
+
+        string letras = string.Join(",", Regex.Matches(texto,padraoLetras));
+        string numeros = string.Join(",", Regex.Matches(texto, padraoNumeros));
+
+        Console.WriteLine("Letras: " +letras);
+        Console.WriteLine("Número: " + numeros);
+        Console.ReadLine();
+    }
+}
